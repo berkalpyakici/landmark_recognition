03c5d5a3e73c78a94e861bcc27fdf794273603c2
Limerickarcher <limaeusarcturus@gmail.com>
2020-11-13 16:52:55 -0600

    moved config into create_split.py

--- src/data_process/create_split.py
+++ src/data_process/create_split.py
@@ -5,7 +5,32 @@ sys.path.insert(0, "..")
 #sys.path.insert(0, "/landmark_recognition/src")
 import pandas as pd
 from tqdm import tqdm
-from config.config import *
+
+import os
+ope = os.path.exists
+opj = os.path.join
+import numpy as np
+import socket
+import warnings
+warnings.filterwarnings('ignore')
+
+sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+hostname = socket.gethostname()
+
+RESULT_DIR     = 'landmark_recognition/results'
+DATA_DIR       = 'landmark_recognition'
+PRETRAINED_DIR = 'landmark_recognition/pretrained'
+
+PI  = np.pi
+INF = np.inf
+EPS = 1e-12
+NUM_CLASSES = 81313
+
+ID      = 'id'
+TARGET  = 'landmark_id'
+CLUSTER = 'cluster'
+SCALE   = 'scale'
+CTARGET = 'landmarks'
 
 def create_whole_train_split(train_meta, split_name):
   train_meta = train_meta.copy()
